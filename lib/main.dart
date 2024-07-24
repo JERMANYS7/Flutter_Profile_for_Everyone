ad3a863ab56bb51b2e9312a832531c3e9d1fb619
JERMANYS7 <boyb5558@gmail.com>
2024-07-24 15:28:26 +0700

    fist_commit

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:profile_app/about.dart';
+import 'package:profile_app/drawer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'My Profile'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Me"),
+      ),
+      drawer: const MyDrawer(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Image.asset("images/profile3.jpg"),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Bawonpak Sriphol",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.facebook,
+                          color: Colors.blue,
+                          size: 40,
+                        ),
+                        Text("facebook"),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.phone,
+                          color: Colors.blue,
+                          size: 40,
+                        ),
+                        Text("Phone"),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.share,
+                          color: Colors.blue,
+                          size: 40,
+                        ),
+                        Text("Share"),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                const Divider(),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "just student",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Get.to(const AboutPage());
+                      },
+                      child: const Text('About Me'),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
